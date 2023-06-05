1bd6bbff9fab819a4e4cffb0de199f79f4c52a3b
RStep1 <stepanrykov.itmo@gmail.com>
2023-06-05 18:51:14 +0300

    add socket and data streams

--- client/src/main/java/user/Client.java
+++ client/src/main/java/user/Client.java
@@ -1,38 +1,47 @@
 package user;
 
-import processing.NBChannelController;
+import processing.TCPExchanger;
 import utility.ServerAnswer;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.nio.channels.SocketChannel;
+import java.net.Socket;
 
 import data.CommandArguments;
 
 public class Client {
-    private static SocketChannel clientSocketChannel;
+    private static Socket clientSocket;
+    private static BufferedInputStream bufferedInputStream;
+    private static BufferedOutputStream bufferedOutputStream;
 
     public Client(String host, int port) throws IOException {
-        clientSocketChannel = SocketChannel.open(new InetSocketAddress(host, port));
+        clientSocket = new Socket(host, port);
+        bufferedInputStream = new BufferedInputStream(new DataInputStream(clientSocket.getInputStream()));
+        bufferedOutputStream = new BufferedOutputStream(new DataOutputStream(clientSocket.getOutputStream()));
     }
 
     public static void stop() {
         try {
-            clientSocketChannel.close();
+            clientSocket.close();
+            bufferedInputStream.close();
+            bufferedOutputStream.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public SocketChannel getSocketChannel() {
-        return clientSocketChannel;
+    public Socket getSocket() {
+        return clientSocket;
     }
 
     public ServerAnswer dataExchange(CommandArguments request) {
         ServerAnswer serverAnswer;
         try {
-            NBChannelController.clientWrite(clientSocketChannel, request);
-            serverAnswer = (ServerAnswer) NBChannelController.clientRead(clientSocketChannel);
+            TCPExchanger.write(bufferedOutputStream, request);
+            serverAnswer = (ServerAnswer) TCPExchanger.read(bufferedInputStream);
         } catch (ClassCastException | IOException e) {
             return null;
         }
